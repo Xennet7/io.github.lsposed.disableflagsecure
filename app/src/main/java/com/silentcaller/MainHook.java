f1b3eac5da582f2e76fbcad29aaf1192571ecd60
XENNET <xennet@rediffmail.com>
2026-05-25 00:18:30 +0530

    Update MainHook.java

--- app/src/main/java/com/silentcaller/MainHook.java
+++ app/src/main/java/com/silentcaller/MainHook.java
@@ -1,7 +1,11 @@
 package com.silentcaller;
 
+import android.app.Activity;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage {
@@ -11,8 +15,29 @@ public class MainHook implements IXposedHookLoadPackage {
             XC_LoadPackage.LoadPackageParam lpparam
     ) {
 
-        XposedBridge.log(
-                "LOADED: " + lpparam.packageName
-        );
+        if (!lpparam.packageName.equals(
+                "com.android.incallui"
+        )) {
+            return;
+        }
+
+        XposedBridge.log("INCALLUI LOADED");
+
+        XposedHelpers.findAndHookMethod(
+                Activity.class,
+                "onResume",
+
+                new XC_MethodHook() {
+
+                    @Override
+                    protected void beforeHookedMethod(
+                            MethodHookParam param
+                    ) {
+
+                        XposedBridge.log(
+                                "Activity resumed in InCallUI"
+                        );
+                    }
+                });
     }
 }

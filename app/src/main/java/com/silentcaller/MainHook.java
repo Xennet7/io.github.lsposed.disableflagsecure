44335bdf855bfc90edcfb9f3c0306e9ec44fb501
XENNET <xennet@rediffmail.com>
2026-05-24 23:49:09 +0530

    Update MainHook.java

--- app/src/main/java/com/silentcaller/MainHook.java
+++ app/src/main/java/com/silentcaller/MainHook.java
@@ -1,11 +1,7 @@
 package com.silentcaller;
 
-import android.telephony.TelephonyManager;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage {
@@ -15,53 +11,8 @@ public class MainHook implements IXposedHookLoadPackage {
             XC_LoadPackage.LoadPackageParam lpparam
     ) {
 
-        if (!lpparam.packageName.equals(
-                "com.android.incallui"
-        )) {
-            return;
-        }
-
-        try {
-
-            Class<?> clazz =
-                    XposedHelpers.findClass(
-                            "com.android.incallui.incomingshow.view.IncomingShowView",
-                            lpparam.classLoader
-                    );
-
-            XposedHelpers.findAndHookMethod(
-                    clazz,
-                    "play",
-
-                    new XC_MethodHook() {
-
-                        @Override
-                        protected void beforeHookedMethod(
-                                MethodHookParam param
-                        ) {
-
-                            try {
-
-                                XposedBridge.log(
-                                        "SilentCaller blocked ringtone"
-                                );
-
-                                param.setResult(null);
-
-                            } catch (Throwable t) {
-
-                                XposedBridge.log(t);
-                            }
-                        }
-                    });
-
-            XposedBridge.log(
-                    "SilentCaller hook loaded"
-            );
-
-        } catch (Throwable t) {
-
-            XposedBridge.log(t);
-        }
+        XposedBridge.log(
+                "LOADED: " + lpparam.packageName
+        );
     }
 }
